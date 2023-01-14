79a47a5bdb225f7c5c937eb0598e800f5cb5a4fe
Jayesh Saini <jayesh.saini.95@gmail.com>
2023-01-14 13:17:22 -0500

    added contact info

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -32,7 +32,7 @@ function ContactMe({}: Props) {
         <div className="space-y-10">
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">+1234567890</p>
+            <p className="text-2xl">+13658223707</p>
           </div>
 
           <div className="flex items-center space-x-5 justify-center">
@@ -42,7 +42,7 @@ function ContactMe({}: Props) {
 
           <div className="flex items-center space-x-5 justify-center">
             <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">123 Developer Lane</p>
+            <p className="text-2xl">Toronto, Ontario</p>
           </div>
         </div>
 
