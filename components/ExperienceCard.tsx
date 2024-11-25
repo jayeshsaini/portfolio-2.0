84a382e94c5cadda1e99f686a554dfb719639eac
Jayesh Saini <jayeshsaini@Jayeshs-MacBook-Air.local>
2024-11-25 15:05:31 -0500

    reversed experience and reduced the height for desktop

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -10,18 +10,18 @@ type Props = {
 
 function ExperienceCard({ experience }: Props) {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-80 cursor-pointer transition-opacity duration-200 overflow-hidden">
+    <article className="flex flex-col rounded-lg items-center space-y-4 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-5 hover:opacity-100 opacity-80 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.img
         initial={{ y: -100, opacity: 0 }}
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        className="w-24 h-24 rounded-full xl:w-[150px] xl:h-[150px] object-cover object-center"
         src={urlFor(experience?.companyImage).url()}
         alt=""
       />
 
-      <div className="px-0 md:px-10">
+      <div className="px-0 md:px-5">
         <h4 className="text-4xl font-light">{experience.jobTitle}</h4>
         <p className="font-bold text-2xl mt-1">{experience.company}</p>
         <div className="flex space-x-2 my-2">
@@ -40,7 +40,7 @@ function ExperienceCard({ experience }: Props) {
             : moment(experience.dateEnded).format("DD/MM/YYYY")}
         </p>
 
-        <ul className="list-disc space-y-4 ml-5 text-lg max-h-96 overflow-y-scroll scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80 pr-5">
+        <ul className="list-disc space-y-2 ml-5 text-lg max-h-96 overflow-y-scroll scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80 pr-5">
           {experience.points.map((point, i) => (
             <li key={i}>{point}</li>
           ))}
